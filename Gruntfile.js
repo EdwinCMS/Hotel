6cff18681fd3ca1b908192660bc35852ff5e6118
Edwin Moreno <edwinc_m1996@hotmail.com>
2020-07-10 23:29:05 -0500

    usando gulp 'PENDIENTE SIN EXITO, verificar error en 'gulp builduu'

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,8 +141,7 @@ module.exports = function(grunt) {
         grunt.loadNpmTasks('grunt-contrib-watch');
         grunt.loadNpmTasks('grunt-contrib-sass');
         grunt.loadNpmTasks('grunt-browser-sync');
-        grunt.loadNpmTasks('grunt-contrib-imagemin');
-        */
+    grunt.loadNpmTasks('grunt-contrib-imagemin');*/
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
     grunt.registerTask('img:compress', ['imagemin']);
